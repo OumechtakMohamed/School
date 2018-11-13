96a5eebc127c6f863eb234e7ed32f954bc6657ec
oumechtak <oumechtak.oumechtak@gmail.com>
2018-11-13 19:43:50 +0100

    Validation Forms

--- SchoolAppTodayFront/SchoolAppTodayFront/src/app/sign-up/sign-up.component.ts
+++ SchoolAppTodayFront/SchoolAppTodayFront/src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../shared/user.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,9 +9,23 @@ import { User } from '../shared/user.model';
 })
 export class SignUpComponent implements OnInit {
   user : User;
+  emailPattern : "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
   constructor() { }
 
   ngOnInit() {
+  	this.resetForm();
+  	  }
+
+  resetForm(form? : NgForm){
+     if(form != null)
+     form.reset();
+     this.user = {
+     	UserName : '',
+     	Password : '',
+     	Email : '',
+     	FirstName : '',
+     	LastName : ''
+     }
   }
 
 }
