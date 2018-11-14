f521990a5e8a9e0bc07a91956401f02596af5bcc
oumechtak <oumechtak.oumechtak@gmail.com>
2018-11-14 18:08:57 +0100

    sign in sign up

--- SchoolAppTodayFront/SchoolAppTodayFront/src/app/app.module.ts
+++ SchoolAppTodayFront/SchoolAppTodayFront/src/app/app.module.ts
@@ -3,23 +3,32 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {UserService} from './shared/user.service';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ToastrModule} from 'ngx-toastr';
+import { UserComponent } from './user/user.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import { appRoutes } from './routes';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    UserComponent,
+    SignInComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     ToastrModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
